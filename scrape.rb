1f4d49438460c40e5e321cb44a4402c9ed94ee7a
uk_sgmt <yuu.moca@gmail.com>
2018-07-18 13:42:26 +0900

    type your commit message here

--- scrape.rb
+++ scrape.rb
@@ -7,23 +7,22 @@ require 'pp'
 
 CSV.open("scrape.csv", "w") do |csv|
 
-  start_number = 680466
-  get_count = 400
+  start_number = 685515
+  get_count = 199
   end_number = start_number + get_count
-  pp "#{get_count}件の処理を開始します"
+  pp "#{get_count + 1}ページ分みてくるわぁ〜＼＼\٩(๑`^´๑)۶//／／"
 
   for path in start_number..end_number do
     url = "https://techplay.jp/event/#{path}/"
     begin
       _html = open(url)
     rescue OpenURI::HTTPError, Errno::ECONNRESET, Errno::ETIMEDOUT
-      pp "#{path} failed"
+      pp "ID:#{path} ページないらしい..."
       sleep 1
       next
     end
 
     doc = Nokogiri::HTML(_html)
-
     title = doc.css('div.title-heading').inner_text
     hizuke = doc.at_css('div.event-day')
     time = doc.at_css('div.event-time')
@@ -34,31 +33,32 @@ CSV.open("scrape.csv", "w") do |csv|
       info_text = info.text()
       hizuke_text = hizuke.text()
       date = hizuke_text.delete("(月)").delete("(火)").delete("(水)").delete("(木)").delete("(金)").delete("(土)").delete("(日)")
-      pp info_text.strip()
+      #pp info_text.strip()
 
       if   info_text.include?("／")
-        apply, total = info_text.delete("人").split("／定員")
+        apply, total = info_text.delete("人").delete("定員").split("／")
 
-        csv << [url, title.strip(), date, time, venue, apply.strip(), total.strip()]
+        csv << [path, url, title.strip(), date, time, venue, apply.strip(), total.strip()]
 
       elsif  info_text.include?("定員")
         apply = " "
         total =   info_text.delete("定員").delete("人").delete("connpass").delete("Doorkeeper").delete("ATND").delete("Peatix").delete("ストアカ")
 
-        csv << [url, title.strip(), date, time, venue, apply.strip(), total.strip()]
+        csv << [path, url, title.strip(), date, time, venue, apply.strip(), total.strip()]
 
       else
-        apply =   info_text.delete("定員").delete("人").delete("connpass").delete("Doorkeeper").delete("ATND").delete("Peatix").delete("ストアカ")
+        apply =   info_text.delete("人").delete("connpass").delete("Doorkeeper").delete("ATND").delete("Peatix").delete("ストアカ")
         total = " "
 
-        csv << [url, title.strip(), date, time, venue, apply.strip(), total.strip()]
+        csv << [path, url, title.strip(), date, time, venue, apply.strip(), total.strip()]
 
       end
 
     end
-    pp "#{path} done"
+    pp "ID:#{path} いけたで(*ﾟ▽ﾟ)ﾉ！"
     sleep 1
 
   end
-  pp "処理を終了しました"
+  pp "終わったから scrape.csv をスプレッドシートにインポートしてな(*ﾟ∀ﾟ*)"
 end
+
